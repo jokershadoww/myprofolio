87521026180b93a8fe803bdfc51abac25f5b76e9
jokershadoww <134153914+jokershadoww@users.noreply.github.com>
2023-09-07 02:25:22 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function home() {
   return (
     <>
       <Head>
-        <title>Shiva | Building beautiful, functional websites for your business</title>
+        <title>Omar JR Profile</title>
         <meta name="description" content="I am a full stack web developer experienced in mern stack " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/shivalogo.ico" />
